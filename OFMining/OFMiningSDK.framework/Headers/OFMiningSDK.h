efa56dcc9bc5a7b000e68406729a55b429509cec
Xie Pengxiang <xiepengxiang@ofbank.com>
2018-04-23 18:32:20 +0800

    Brief: pod lib init
    Jira:
    Desc:
    Reviewer:
    Signed-off-by: Xie pengxiang <xiepengxiang@ofbank.com>

---
+++ OFMining/OFMiningSDK.framework/Headers/OFMiningSDK.h
@@ -0,0 +1,21 @@
+//
+//  OFMiningSDK.h
+//  OFMiningSDK
+//
+//  Created by xiepengxiang on 2018/4/23.
+//  Copyright © 2018年 OFBank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OFMiningSDK.
+FOUNDATION_EXPORT double OFMiningSDKVersionNumber;
+
+//! Project version string for OFMiningSDK.
+FOUNDATION_EXPORT const unsigned char OFMiningSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OFMiningSDK/PublicHeader.h>
+
+#import <OFMiningSDK/OFLoginTest.h>
+#import <OFMiningSDK/OFMiningTest.h>
+
---
+++ OFMining/OFMiningSDK.framework/Headers/OFMiningTest.h
@@ -0,0 +1,15 @@
+//
+//  OFMiningTest.h
+//  OFMiningSDK
+//
+//  Created by xiepengxiang on 2018/4/23.
+//  Copyright © 2018年 OFBank. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OFMiningTest : NSObject
+
+- (void)testMining;
+
+@end
